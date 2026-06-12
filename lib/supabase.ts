f723cbf790a53f6b02b490a1198aef72634d3bc1
deniz <denisnotspreme@gmail.com>
2026-06-12 19:05:06 +0300

    Delete lib directory

--- lib/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.https://hztnrujpybadvbuvovzv.supabase.co;
-const supabaseAnonKey = process.env.sb_publishable_omJyUWjAa2ujOGItr9JjZg_gAPRmXgK;
-
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
-);
